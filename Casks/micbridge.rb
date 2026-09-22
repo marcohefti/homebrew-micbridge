aab7b609ee0e1e9569bc5292aef10a9cd298ef53
marcohefti <marco@heftiweb.ch>
2026-09-22 21:02:27 +0700

    cask(micbridge): 0.2.0

--- Casks/micbridge.rb
+++ Casks/micbridge.rb
@@ -1,8 +1,8 @@
 cask "micbridge" do
-  version "0.1.0"
-  sha256 "c923a946916a3c5345b414185fe92033005cb0e2679375922f9562c79da8ef20"
+  version "0.2.0"
+  sha256 "023d3f567204bf1b4cf24fc375dfd6ead12dc522d189d7fea3dc5eb8c067200d"
 
-  url "https://github.com/marcohefti/mac-virtual-mic-bridge/releases/download/v0.1.0/MicBridge-0.1.0.zip"
+  url "https://github.com/marcohefti/mac-virtual-mic-bridge/releases/download/v0.2.0/MicBridge-0.2.0.zip"
   name "MicBridge"
   desc "Resilient macOS virtual microphone bridge"
   homepage "https://github.com/marcohefti/mac-virtual-mic-bridge"
